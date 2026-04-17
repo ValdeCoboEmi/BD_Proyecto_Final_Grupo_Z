ed59488ea3cf2f9e352ef507b967ba07fc8eeccc
ValdeCoboEmi <00003525@uca.edu.sv>
2026-04-16 22:12:18 -0600

    :tada: start(): Crear archivos bases de los scripts

---
+++ script_ddl.sql
@@ -0,0 +1,12 @@
+create database db_sistemas_reservas_hotel;
+
+create table hotel();
+create table tipo_habitacion();
+create table habitacion();
+create table huesped();
+create table reservacion();
+create table checking();
+create table servicio();
+create table consumo_servicio();
+create table empleado();
+create table factura();
